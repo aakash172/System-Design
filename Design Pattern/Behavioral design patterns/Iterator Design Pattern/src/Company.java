9ecda807aa864b2407923be3e93d01c3d64b9c56
aakash <aakashchaudhary172m@gmail.com>
2025-01-04 20:14:43 +0530

    Iterator Design Pattern

---
+++ Design Pattern/Behavioral design patterns/Iterator Design Pattern/src/Company.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+class Company implements Aggregate<Employee> {
+    private List<Employee> employees;
+
+    public Company(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public Iterator<Employee> createIterator() {
+        return new EmployeeIterator(employees);
+    }
+}
---
+++ Design Pattern/Behavioral design patterns/Iterator Design Pattern/src/Employee.java
@@ -0,0 +1,13 @@
+class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
